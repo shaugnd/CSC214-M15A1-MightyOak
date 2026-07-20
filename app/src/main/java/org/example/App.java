6fe6c9fa5a71a0ee171aed07e8b927fe7ed122cd
Shaugn Davenport <shaugnd@gmail.com>
2026-07-20 17:42:57 -0500

    Initial commit

---
+++ app/src/main/java/org/example/App.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class App {
+    public static void main(String[] args) {
+      System.out.println("This is where you will put the driver program! It will run the source code that you write for the lab.");
+      System.out.println("Create separate files when writing your source code. Don't put all of your source code in this file!");
+      System.out.println("You can delete these println statements and replace them with your driver program.");
+    }
+}
